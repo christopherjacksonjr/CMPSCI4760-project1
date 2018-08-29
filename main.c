f56bdf0cd0f67430fdad2b15aef425bf7a2a9c2a
christopher jackson <jackson@hoare7.cs.umsl.edu>
2018-08-28 21:56:35 -0500

    getopt taking three options

--- main.c
+++ main.c
@@ -4,11 +4,39 @@
 
 int main (int argc, char *argv[]) 
 {
-	pid_t childpid = 0;
+	int c;
+
+	while ((c = getopt (argc, argv, "n:hp")) != -1)
+	{
+		switch(c)
+		{
+			case 'n':
+				printf("Variable option chosen.\n");
+				break;
+			case 'h':
+				printf("Help option chosen.\n");
+				break;
+			case 'p':
+				printf("perror option chosen.\n");
+				break;
+			case '?':
+				if (optopt == 'c')
+          			fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+        			else if (isprint (optopt))
+          			fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+        			else
+          			fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+        			return 1;
+			default:
+				abort();
+		}
+	}
+
+	/*pid_t childpid = 0;
    	int i, n;
    	
 	if (argc != 2)
-	{   /* check for valid number of command-line arguments */
+	{
       		fprintf(stderr, "Usage: %s processes\n", argv[0]);
       		return 1;
    	}
@@ -18,5 +46,5 @@ int main (int argc, char *argv[])
         		break;
    	fprintf(stderr, "i:%d  process ID:%ld  parent ID:%ld  child ID:%ld\n",
            i, (long)getpid(), (long)getppid(), (long)childpid);
-	return 0; 
+	return 0;*/ 
 }
